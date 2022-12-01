f50092683dc4594818320511bbd4a3523c29bde6
Ben Huggins <benshuggins@gmail.com>
2022-12-01 12:53:29 -0800

    comments

--- HudView/HudView.swift
+++ HudView/HudView.swift
@@ -25,10 +25,8 @@ class HudView: UIView {
     override func draw(_ rect: CGRect) {
         let boxWidth: CGFloat = 96
         let boxHeight: CGFloat = 96
-        
         let boxRect = CGRect(x: round((bounds.size.width - boxWidth)/2), y: round((bounds.size.height - boxHeight)/2), width: boxWidth, height: boxHeight)
         let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
-       // UIColor(white: 0.3, alpha: 0.8).setFill()
         UIColor(red: 0, green: 0.8, blue: 0, alpha: 0.8).setFill()
         roundedRect.fill()
         
@@ -38,22 +36,17 @@ class HudView: UIView {
             image.draw(at: imagePoint)
         }
         
-        
         // This adds the text "tagged" to the hudview
-        
         let attribs = [
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
             NSAttributedString.Key.foregroundColor: UIColor.white ]
-            
         let textSize = text.size(withAttributes: attribs)
-        
         let textPoint = CGPoint(x: center.x - round(textSize.width/2) , y: center.y - round(textSize.height-2) + boxHeight/4)
         text.draw(at: textPoint, withAttributes: attribs)
-        
     }
     
+    // This animates the presentation of the hudview
     func show(animated: Bool) {
-        
         if animated {
             alpha = 0
             transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
@@ -65,7 +58,7 @@ class HudView: UIView {
             }, completion: nil)
         }
     }
-    
+        // This dismisses the hudview when it gets back to the superview
     func hide() {
         superview?.isUserInteractionEnabled = true
         removeFromSuperview()
