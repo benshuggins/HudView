860a02c5334a4c78bb381b98f59a38e9e4816cd0
Ben Huggins <benshuggins@gmail.com>
2022-12-01 10:25:31 -0800

    Initial Commit

---
+++ HudView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HudView
+//
+//  Created by Ben Huggins on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
